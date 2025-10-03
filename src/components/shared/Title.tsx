b7884b357d0cccf143a4b6a7a069192f2d2d7c55
jmauricioaf <jmauricio.a.ferreira@gmail.com>
2025-10-03 00:22:17 -0300

    ✨ enhance AboutMeSection layout and styling for better responsiveness, update Title component for improved text sizing

--- src/components/shared/Title.tsx
+++ src/components/shared/Title.tsx
@@ -12,7 +12,7 @@ export const Title = (props: TextProps) => {
 
   return (
     <h1
-      className={`font-porsha text-custom-24 sm:text-custom-96 leading-[15rem] bg-gradient-to-r from-title-gradient-start to-title-gradient-end bg-clip-text text-transparent ${classes}`}
+      className={`font-porsha text-6xl sm:text-6xl md:text-7xl lg:text-custom-96 xl:text-custom-96 leading-[4rem] sm:leading-[6rem] md:leading-[8rem] lg:leading-[10rem] xl:leading-[15rem] bg-gradient-to-r from-title-gradient-start to-title-gradient-end bg-clip-text text-transparent ${classes}`}
     >
       {children}
     </h1>
